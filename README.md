96b37f49c1b0661a51639ca21bc1b95604008c06
Shermam Miranda <shermam.miranda@hotmail.com>
2018-07-05 17:53:14 -0300

    extracting more data from the PDM file

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # pdm-to-json
 Converts pdm files to JSON objects
 
-https://www.codeproject.com/Articles/27431/Writing-Your-Own-RTF-Converter
+
--- src/index.js
+++ src/index.js
@@ -7,68 +7,132 @@ const fs = require('fs');
 const writeFile = util.promisify(fs.writeFile);
 const readFile = util.promisify(fs.readFile);
 const parseString = util.promisify(parser.parseString);
+const rtfToHTMLLib = util.promisify(require('@iarna/rtf-to-html').fromString);
 
 module.exports = function (fileName) {
 
     return readFile(fileName, 'utf8')
         .then(parseString)
         .then(getPdmInfo)
-        .catch(e => null);
+        .catch(console.log);
 }
 
-function getPdmInfo(parsedJson) {
-    const tables = getTablesObject(parsedJson);
-    return formatTables(tables);
-}
+async function getPdmInfo(parsedJson) {
+    const model = parsedJson['Model']['o:RootObject'][0]['c:Children'][0]['o:Model'][0];
+    const tables = model['c:Tables'][0]['o:Table'];
+    const parsedModel = {};
 
-function getTablesObject(object) {
+    for (const table of tables) {
+        const ref = table['$']['id'];
+        const name = codify(table['a:Name'][0]);
+        const code = table['a:Code'][0];
+        const conceptualName = table['a:Name'][0];
+        const comment = table['a:Comment'];
+        const description = await rtfToHTML(table['a:Description']);
+        const primaryKeyArray = table["c:PrimaryKey"][0]["o:Key"];
+        const columns = await Promise.all(table["c:Columns"][0]["o:Column"].map(async column => {
+            return {
+                ref: column['$']['id'],
+                name: codify(column['a:Name'][0]),
+                code: column['a:Code'][0],
+                conceptualName: column['a:Name'][0],
+                description: await rtfToHTML(column['a:Description']),
+                dataType: column['a:DataType'][0],
+                isIdentity: !!Number(column['a:Identity']),
+                isMandatory: !!Number(column['a:Mandatory']),
+                isPrimaryKey: !!primaryKeyArray.find(k => k['$']['Ref'] === column['$']['id'])
+            }
+        }));
 
-    if (typeof object !== 'object') {
-        return;
+        parsedModel[code] = {
+            ref, name, code, conceptualName, comment, description, columns
+        };
     }
 
-    for (const key in object) {
-        if (typeof key === "string" && key.includes('Tables')) {
-            return object[key][0]
+    return parsedModel;
+}
+
+async function rtfToHTML(arr) {
+    try {
+        if (arr && arr.length) {
+            return await rtfToHTMLLib(arr[0]);
         }
-
-        const ret = getTablesObject(object[key]);
-
-        if (ret) return ret;
+    } catch (error) {
+        return await arr;
     }
 }
 
-function formatTables(tables) {
-    const formattedTables = {};
-    const tablesArray = getByName(tables, 'Table');
-    tablesArray.forEach(t => {
-        const name = getFormattedName(t);
-        const code = getByName(t, 'Code')[0];
-        const columns = getByName(getByName(t, 'Columns')[0], 'Column')
-            .map(formatColumn);
-        formattedTables[code] = { name, code, columns };
-    });
-
-    return formattedTables;
-}
-
-function formatColumn(c) {
-    return {
-        name: getFormattedName(c),
-        code: getByName(c, 'Code')[0]
-    }
-}
-
-function getFormattedName(obj) {
-    return removeDiacritics(getByName(obj, 'Name')[0])
+function codify(str) {
+    return removeDiacritics(str)
         .replace(/ /g, "")
         .replace(/\W/g, "");
 }
 
-function getByName(obj, name) {
-    for (const key in obj) {
-        if (key.includes(name)) {
-            return obj[key];
+function findColumnByRef(parsedTable, ref) {
+    return parsedTable.columns.find(c => c.ref === ref);
+}
+
+function findTableByRef(parsedModel, ref) {
+    for (const key in parsedModel) {
+        if (parsedModel[key].ref === ref) {
+            return parsedModel[key];
         }
     }
 }
+
+// function getPdmInfo(parsedJson) {
+//     const tables = getTablesObject(parsedJson);
+//     return formatTables(tables);
+// }
+
+// function getTablesObject(object) {
+
+//     if (typeof object !== 'object') {
+//         return;
+//     }
+
+//     for (const key in object) {
+//         if (typeof key === "string" && key.includes('Tables')) {
+//             return object[key][0]
+//         }
+
+//         const ret = getTablesObject(object[key]);
+
+//         if (ret) return ret;
+//     }
+// }
+
+// function formatTables(tables) {
+//     const formattedTables = {};
+//     const tablesArray = getByName(tables, 'Table');
+//     tablesArray.forEach(t => {
+//         const name = getFormattedName(t);
+//         const code = getByName(t, 'Code')[0];
+//         const columns = getByName(getByName(t, 'Columns')[0], 'Column')
+//             .map(formatColumn);
+//         formattedTables[code] = { name, code, columns };
+//     });
+
+//     return formattedTables;
+// }
+
+// function formatColumn(c) {
+//     return {
+//         name: getFormattedName(c),
+//         code: getByName(c, 'Code')[0]
+//     }
+// }
+
+// function getFormattedName(obj) {
+//     return removeDiacritics(getByName(obj, 'Name')[0])
+//         .replace(/ /g, "")
+//         .replace(/\W/g, "");
+// }
+
+// function getByName(obj, name) {
+//     for (const key in obj) {
+//         if (key.includes(name)) {
+//             return obj[key];
+//         }
+//     }
+// }
